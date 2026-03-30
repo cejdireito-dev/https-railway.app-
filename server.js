830a54bd79e4f8a4c972fb1e36afab0aa114cb34
cejdireito-dev <cej.direito@gmail.com>
2026-03-30 08:08:35 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
+const express = require("express");
+const app = express();
 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend funcionando 🚀");
+});
+
+app.post("/validate-license", (req, res) => {
+  const { code } = req.body;
+
+  if (code === "123") {
+    return res.json({ valid: true });
+  }
+
+  res.json({ valid: false });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
+});
